eb6a7cdb42b8920978a5dfafc6faec974370c1a9
Dusan Matejka <matejkadusan32@gmail.com>
2016-05-05 13:39:37 +0200

    added basic ansible module

---
+++ ansible-module-basic/library/my_module.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+
+import os
+from ansible.module_utils.basic import *
+
+
+def create_test_file(name, content):
+    with open('/tmp/' + name, 'w') as f:
+        f.write(content)
+
+
+def delete_test_file(name):
+    if os.path.exists('/tmp/' + name):
+        os.remove('/tmp/' + name)
+
+
+if __name__ == '__main__':
+    module = AnsibleModule(
+        argument_spec = dict(
+            state = dict(default='present', choices=['present', 'absent']),
+            name = dict(required=True),
+            content = dict(type='str')))
+
+    result = module.params.copy()
+    result['changed'] = False
+
+    if module.params['state'] == 'present':
+        create_test_file(module.params['name'], module.params['content'])
+        result['changed'] = True
+
+    elif module.params['state'] == 'absent':
+        delete_test_file(module.params['name'])
+        result['changed'] = True
+
+    module.exit_json(**result)
