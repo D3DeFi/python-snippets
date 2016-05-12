d23a8b2b98ce991dc8473e6c23b686977770925f
Dusan Matejka <matejkadusan32@gmail.com>
2016-05-12 09:57:26 +0200

    finished argument parsing logic

--- perfunction-argparse/README.md
+++ perfunction-argparse/README.md
@@ -1,5 +1,5 @@
 Description
 -----------
-Snippet shows how to move command line argument definitions to function decorator
+Snippet shows how to move command line argument definitions into function decorator.
 
-Inspired by OpenStack Nova project (nova/nova/cmd/manage.py), but simplified
+Inspired by OpenStack Nova project (nova/nova/cmd/manage.py)
--- perfunction-argparse/app.py
+++ perfunction-argparse/app.py
@@ -12,6 +12,7 @@ ITEMS = {
 
 # decorator for argument definitions
 def args(*args, **kwargs):
+    """Attaches argument to a __dict__ attribute within a specific function or method."""
     def _decorator(func):
         func.__dict__.setdefault('args', []).insert(0, (args, kwargs))
         return func
@@ -22,25 +23,36 @@ def args(*args, **kwargs):
 class ListActions(object):
     """list sub-command help."""
 
+    def router(self, args):
+        self.list_items(args.pretty or None)
+
     @args('--pretty', action='store_true', help='display pretty output')
     def list_items(self, pretty):
         if pretty:
             for item in ITEMS:
                 print(item)
+        else:
+            print(ITEMS)
 
 # Example command with methods decored
 # docstring of a class will be used as a subcommand help
 class ShowActions(object):
     """show sub-command help."""
 
+    # specify arguments only once
     @args('--name', type=str, metavar='<name>', help='name of a displayed item')
-    def show_item(self, name):
-        print(ITEMS.get(key, None) or 'Key not found!')
+    def router(self, args):
+        if 'type' in args.__dict__ and args.__dict__['type'] == True:
+            self.show_item_type(args.name)
+        else:
+            self.show_item(args.name)
 
-    @args('--name', type=str, metavar='<name>', help='name of a displayed item')
+    def show_item(self, name):
+        print(ITEMS.get(name, None) or 'Key not found!')
+
     @args('--type', action='store_true', help='display type of a shown item')
-    def show_item_type(self, name, type):
-        print(type(ITEMS.get(key, None)) or 'Key not found!')
+    def show_item_type(self, name):
+        print(type(ITEMS.get(name, None)) or 'Key not found!')
 
 # definition in which commands to parse arguments
 CATEGORIES = {
@@ -48,19 +60,39 @@ CATEGORIES = {
     'list': ListActions
 }
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(help='sub-command help')
+def get_argument_parser(description=None):
+    """Iterates over function's or method's __dict__['args'] variable to load arguments into arg parser."""
+    parser = argparse.ArgumentParser(description=description)
+    subparsers = parser.add_subparsers(help='sub-command help', dest='subcommand')
     for category in CATEGORIES:
         obj = CATEGORIES[category]()
+
         # use docstring as a help and define subcommand in argument parser
         desc = getattr(obj, '__doc__', None)
         sub_parser = subparsers.add_parser(category, help=desc)
 
         # get all class public methods
-        category_methods = [getattr(obj, m) for m in dir(m) if callable(getattr(obj, m)) and not m.startswith('_')]
+        category_methods = [getattr(obj, m) for m in dir(obj) if callable(getattr(obj, m)) and not m.startswith('_')]
         # iterate over callable methods and load defined arguments
-        for args, kwargs in [getattr(func, 'args', []) for func in category_methods]:
-            pass
+        arguments = dict()
+        for method in category_methods:
+            for args, kwargs in getattr(method, 'args', []):
+                # if argument does not have 'dest' parameter, it's name will be used instead
+                dest = kwargs.get('dest', None) or args[0].lstrip('-')
+                if dest not in arguments:
+                    sub_parser.add_argument(*args, **kwargs)
 
+                # make sure only first argument is used if two or more are found with the same name
+                arguments.setdefault(dest, [args, kwargs])
+
+    return parser
+
+
+if __name__ == '__main__':
+    parser = get_argument_parser(description='Example parser')
     args = parser.parse_args()
+
+    # load appriopiate category, argparse will handle correct output for us
+    # TODO: create better logic for matching methods/functions with arguments
+    category = CATEGORIES[args.subcommand]()
+    category.router(args)
